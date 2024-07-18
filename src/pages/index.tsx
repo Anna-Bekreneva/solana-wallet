1ef85477828cbca40c43846f1877a4366f324176
Anna-Bekreneva <anjabekreneva@gmail.com>
2024-07-18 17:39:06 +0300

    refactor

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,20 @@
+'use client'
+
 import Link from "next/link";
 import {APP_ROUTES} from "@/shared";
+import s from './index.module.scss'
 
 export default function Home() {
   return (
-    <div>
-      <Link href={APP_ROUTES.wallet}>Кошелёк</Link>
-      <Link href={APP_ROUTES.transactions}>Транзакции</Link>
-    </div>
+    <nav className={s.nav}>
+      <ul className={s.list}>
+        <li>
+          <Link className={s.link} href={APP_ROUTES.wallet}>Кошелёк</Link>
+        </li>
+        <li>
+          <Link className={s.link} href={APP_ROUTES.transactions}>Транзакции</Link>
+        </li>
+      </ul>
+    </nav>
   );
 }
